e84c20a91ea6a4973f1fe6bef54d9ca8c19db150
Thomas Roccia <thomas.roccia@gmail.com>
2020-09-14 08:52:39 +0200

    Create macdetect.go

---
+++ macdetect.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net"
+    "strings"
+)
+
+func getMacAddr() ([]string, error) {
+    ifas, err := net.Interfaces()
+    if err != nil {
+        return nil, err
+    }
+    var as []string
+    for _, ifa := range ifas {
+        a := ifa.HardwareAddr.String()
+        if a != "" {
+            as = append(as, a)
+        }
+    }
+    return as, nil
+}
+
+func main() {
+    // Blacklist VM mac address
+    var macvm = []string{"08:00:27", "00:0C:29", "00:1C:14", "00:50:56", "00:05:69"}
+
+    as, err := getMacAddr()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    for i, s:= range macvm {
+        for _, a := range as {
+            str := strings.ToUpper(a)
+            if str[0:8] == s[0:8] {
+                fmt.Println("VM detected!")
+				fmt.Println(i, s)
+            } 
+         }
+    }
+}
